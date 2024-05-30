264c8a9e56e4138bf02e1fdd70067410c8c5318c
Giovanni Torelli Leite Walter <giovanni.walter@outlook.com>
2024-05-29 22:10:49 -0300

    Finished day 3 tutorials

--- practical-go/github/github.go
+++ practical-go/github/github.go
@@ -1,24 +1,39 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
+	"time"
 )
 
 func main() {
-	name, repos, _ := githubInfo("torelli")
-	fmt.Printf("Hi, my name is %s and I have %d public repos\n", name, repos)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Millisecond)
+	defer cancel()
+	name, repos, err := githubInfo(ctx, "torelli")
+	if err != nil {
+		fmt.Printf(err.Error())
+	} else {
+		fmt.Printf("Hi, my name is %s and I have %d public repos\n", name, repos)
+	}
 }
 
 // githubInfo returns name and number of public repos for login
-func githubInfo(login string) (string, int, error) {
+func githubInfo(ctx context.Context, login string) (string, int, error) {
 	url := "https://api.github.com/users/" + url.PathEscape(login)
-	resp, err := http.Get(url)
+	// resp, err := http.Get(url)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
 		return "", 0, err
 	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", 0, err
+	}
+
 	if resp.StatusCode != http.StatusOK {
 		return "", 0, fmt.Errorf("%#v - %s", url, resp.Status)
 	}
