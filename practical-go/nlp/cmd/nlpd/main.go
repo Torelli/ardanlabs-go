d1a03b0a5e90cca87fa3daecf99f8fc0dd0ad030
Giovanni Torelli Leite Walter <giovanni.walter@outlook.com>
2024-06-13 18:26:56 -0300

    Finished day 4 tutorials

---
+++ practical-go/nlp/cmd/nlpd/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"encoding/json"
+	"expvar"
+	_ "expvar"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+
+	"github.com/353solutions/nlp"
+	"github.com/353solutions/nlp/stemmer"
+)
+
+var (
+	numTok = expvar.NewInt("tokenize.calls")
+)
+
+func main() {
+	// Create server (dependency injection)
+	logger := log.New(log.Writer(), "[nlpd] ", log.LstdFlags|log.Lshortfile)
+	s := Server{
+		logger: logger, // dependency injection
+	}
+	// routing
+	// /health is an exact match
+	// /health/ is a prefix match
+	/*
+		http.HandleFunc("/health", healthHandler)
+		http.HandleFunc("/tokenize", tokenizeHandler)
+	*/
+	r := mux.NewRouter()
+	r.HandleFunc("/health", s.healthHandler).Methods(http.MethodGet)
+	r.HandleFunc("/tokenize", s.tokenizeHandler).Methods(http.MethodPost)
+	r.HandleFunc("/stem/{word}", s.stemHandler).Methods(http.MethodGet)
+	http.Handle("/", r)
+
+	// run server
+	addr := os.Getenv("NLPD_ADDR")
+	if addr == "" {
+		addr = ":8080"
+	}
+	s.logger.Printf("server starting on %s", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Fatalf("error: %s", err)
+	}
+}
+
+type Server struct {
+	logger *log.Logger
+}
+
+func (s *Server) stemHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	word := vars["word"]
+	stem := stemmer.Stem(word)
+	fmt.Fprintln(w, stem)
+}
+
+func (s *Server) tokenizeHandler(w http.ResponseWriter, r *http.Request) {
+	/* Before gorilla/mux
+	if r.Method != http.MethodPost {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+	}
+	*/
+
+	numTok.Add(1)
+
+	// Step 1: Get, convert and validate the data
+	defer r.Body.Close()
+	rdr := io.LimitReader(r.Body, 1_000_000)
+	data, err := io.ReadAll(rdr)
+	if err != nil {
+		s.logger.Printf("error: can't read - %s", err)
+		http.Error(w, "can't read", http.StatusBadRequest)
+		return
+	}
+
+	if len(data) == 0 {
+		http.Error(w, "missing data", http.StatusBadRequest)
+		return
+	}
+
+	text := string(data)
+
+	// Step 2: Work
+	tokens := nlp.Tokenize(text)
+
+	// Step 3: Encode and emit output
+	resp := map[string]any{
+		"tokens": tokens,
+	}
+	// You can also do:
+	// err = json.NewEncoder(w).Encode(resp)
+	data, err = json.Marshal(resp)
+	if err != nil {
+		http.Error(w, "can't encode", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
+}
+
+func (s *Server) healthHandler(w http.ResponseWriter, r *http.Request) {
+	// TODO: Run a health check
+	fmt.Fprintln(w, "OK")
+}
---
+++ practical-go/nlp/go.mod
@@ -0,0 +1,18 @@
+module github.com/353solutions/nlp
+
+go 1.22.3
+
+require (
+	// Prod requirements
+	github.com/gorilla/mux v1.8.1
+
+	// Dev requirements
+	github.com/BurntSushi/toml v1.4.0
+	github.com/stretchr/testify v1.9.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
